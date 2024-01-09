f8fc0f57313ebcbc6d5e95f5554d1d19722edfc7
Debby696 <idowuopeyemi696@gmail.com>
2024-01-09 14:57:11 +0000

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,10 @@
 const getListStudentIds = (listStudents) => {
   let listofStudentIds = [];
   if (!(listStudents instanceof Array)) {
-    return listSoftudentIds;
+    return listofStudentIds;
   }
   listofStudentIds = listStudents.map((student) => student.id);
   return listofStudentIds;
 };
 
-export default getListofStudentIds;
+export default getListStudentIds;
