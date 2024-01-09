8d9f2882ff2373a3ebd1b1076bd03ba9b6280833
Debby696 <idowuopeyemi696@gmail.com>
2024-01-09 18:28:06 +0000

    update

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer)
-  if (position > intallArray.length) throw new Error('Position outside range');
-  intallArray[position] = value;
+  const int8Array = new Int8Array(buffer); // Fix typo here
+  if (position >= int8Array.length) throw new Error('Position outside range'); // Use >= to include the last index
+  int8Array[position] = value;
   return new DataView(buffer);
 };
 
